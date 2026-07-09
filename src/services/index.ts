92ba72e1299689869d320fb832997882423e8793
StevenPradhan <steven.sayala295@gmail.com>
2026-07-09 18:59:41 +0530

    feat: add services index for barrel export

---
+++ src/services/index.ts
@@ -0,0 +1,8 @@
+/**
+ * Index file for services barrel export
+ */
+
+export * from "./package.service";
+export * from "./booking.service";
+export * from "./review.service";
+export * from "./payment.service";
